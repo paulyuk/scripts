636611381fd3b55d7db14d0ff33510c84eabe0f6
paulyuk <paulyuk@microsoft.com>
2019-03-13 23:40:26 -0700

    fixed RG name

--- aks/aks-start.sh
+++ aks/aks-start.sh
@@ -1,3 +1,3 @@
-echo starting AKS cluster in rg 'pyk8s'
+echo starting AKS cluster in rg 'MC_pyk8s_pyaksmini_westus2'
 
-az vm start --ids $(az vm list --resource-group pyk8s --query "[].id" -o tsv)
+az vm start --ids $(az vm list --resource-group MC_pyk8s_pyaksmini_westus2 --query "[].id" -o tsv)
--- aks/aks-stop.sh
+++ aks/aks-stop.sh
@@ -1,4 +1,4 @@
-echo stopping AKS cluster in rg 'pyk8s'
+echo stopping AKS cluster in rg 'MC_pyk8s_pyaksmini_westus2'
 
-az vm stop --ids $(az vm list --resource-group pyk8s --query "[].id" -o tsv)
+az vm stop --ids $(az vm list --resource-group MC_pyk8s_pyaksmini_westus2 --query "[].id" -o tsv)
 
