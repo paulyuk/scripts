29918b38d5f4aa30f998d7119b2192ba22989766
paulyuk <paulyuk@microsoft.com>
2019-03-14 01:28:18 -0700

    Stop now does stop (deallocate)

--- aks/aks-stop.sh
+++ aks/aks-stop.sh
@@ -1,4 +1,4 @@
 echo stopping AKS cluster in rg 'MC_pyk8s_pyaksmini_westus2'
 
-az vm stop --ids $(az vm list --resource-group MC_pyk8s_pyaksmini_westus2 --query "[].id" -o tsv)
+az vm deallocate --ids $(az vm list --resource-group MC_pyk8s_pyaksmini_westus2 --query "[].id" -o tsv)
 
